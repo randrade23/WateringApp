668fd8d76f7f2ec8f2331a5ac867658551c645e3
Rui Andrade <ruiandrade@outlook.com>
2017-04-03 15:33:14 +0100

    corrected app name, added plant status activity

---
+++ app/src/main/java/pt/ruiandrade/watering/PlantStatusActivity.java
@@ -0,0 +1,13 @@
+package pt.ruiandrade.watering;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_status);
+    }
+}
